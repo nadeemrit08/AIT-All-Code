0a0ee35bbb89cbcd2b05cf808810933968a5e87d
Nadeem <nadeem.ahmad90582@gmail.com>
2026-05-19 21:59:02 +0530

    JPQL Application Added

---
+++ hibernate-7am/workspace(2026-03)/05-JPQL-App/src/main/java/in/nadeem/entity/EmployeeEntity.java
@@ -0,0 +1,43 @@
+package in.nadeem.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+
+public class EmployeeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String dept;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+}
---
+++ hibernate-7am/workspace/05-JPQL-App/src/main/java/in/nadeem/entity/EmployeeEntity.java
@@ -0,0 +1,8 @@
+package in.nadeem.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class EmployeeEntity {
+
+}
